d0dbaecc09ce542a54daa88e031931ce066aebc9
Halvor Aarnes  Krog <halvor.krog@sintef.no>
2024-05-02 11:42:29 +0200

    now the minimum working example is in examples, and things are running

--- examples/minimum_working_example_dev.jl
+++ examples/minimum_working_example_dev.jl
@@ -1,9 +1,18 @@
+using Pkg
+
+# Activate the local environment including EnergyModelsBase, HiGHS, PrettyTables
+# Pkg.activate(@__DIR__)
+# Install the dependencies.
+Pkg.activate("test")
+
 using HiGHS
 using PrettyTables
 import JuMP as jp
 #import EnergyModelsBase as emb
+using EnergyModelsBase
+using TimeStruct
+import EnergyModelsRecHorizon as EMRH
 
-import EnergyModelsRecHorizon as emrh
 
 function cost_to_go_func(opt_vars_input::Vector{jp.VariableRef})
     
@@ -16,17 +25,17 @@ end
 
 function run_case(op_number, demand_profile; case_config = "standard")
     #Define resources with their emission intensities
-    power = emrh.ResourceCarrier("power", 
+    power = ResourceCarrier("power", 
     0.0 #tCO2/MWh
     )
-    co2 = emrh.ResourceEmit("co2", 
+    co2 = ResourceEmit("co2", 
     1.0 #tCO2/MWh
     )
     products = [power, co2]
 
     #define time structure
     op_duration = 2 # duration of each operational period
-    operational_periods = emrh.SimpleTimes(op_number, op_duration)
+    operational_periods = SimpleTimes(op_number, op_duration)
 
     #number of operational periods within one strategic period
     op_per_strat = op_duration*op_number
@@ -34,48 +43,48 @@ function run_case(op_number, demand_profile; case_config = "standard")
 
     if true
         #create time structure
-        T = emrh.TwoLevel(1, #number of strategic periods
+        T = TwoLevel(1, #number of strategic periods
         1, #duration of strategic period
         operational_periods; #operational period
         op_per_strat)
 
-        T2 = emrh.TwoLevel([operational_periods, operational_periods])
+        T2 = TwoLevel([operational_periods, operational_periods])
     else 
         #purely operational period. However, this does not work for some reason (check_timeprofiles fails)
-        T = emrh.SimpleTimes(op_number, op_duration)
+        T = SimpleTimes(op_number, op_duration)
     end
 
     #define the model
-    model = emrh.OperationalModel(
-        Dict(co2 => emrh.FixedProfile(10)), #upper bound for CO2 in t/8h
-        Dict(co2 => emrh.FixedProfile(0)), # emission price for CO2 in EUR/t
+    model = OperationalModel(
+        Dict(co2 => FixedProfile(10)), #upper bound for CO2 in t/8h
+        Dict(co2 => FixedProfile(0)), # emission price for CO2 in EUR/t
         co2    
     )
 
     #create individual nodes of the system
     nodes = [
-        emrh.RefSource(
+        RefSource(
             "electricity source", #Node id or name
-            emrh.FixedProfile(1e12), #Capacity in MW (Time profile)
-            emrh.FixedProfile(30), #variable OPEX (time structure) in EUR/MW
-            emrh.FixedProfile(0), #Fixed OPEN in EUR/8h
+            FixedProfile(1e12), #Capacity in MW (Time profile)
+            FixedProfile(30), #variable OPEX (time structure) in EUR/MW
+            FixedProfile(0), #Fixed OPEN in EUR/8h
             Dict(power => 1), #output from the node (key must be a :<resource, value a :<real), here it is power
         ),
-        emrh.RefSink(
+        RefSink(
             "electricity demand", #node ID or name
-            emrh.OperationalProfile(demand_profile), #demand in MW (time profile)
-            # emrh.OperationalProfile([20, 30, 40, 30]), #demand in MW (time profile)
-            Dict(:surplus => emrh.FixedProfile(0), :deficit => emrh.FixedProfile(1e6)), #surplus and deficit penalty for the node in EUR/MWh
+            OperationalProfile(demand_profile), #demand in MW (time profile)
+            # OperationalProfile([20, 30, 40, 30]), #demand in MW (time profile)
+            Dict(:surplus => FixedProfile(0), :deficit => FixedProfile(1e6)), #surplus and deficit penalty for the node in EUR/MWh
             Dict(power => 1), #energy demand and corresponding ratio
         )
     ]
 
     #connect the nodes with links
-    links = [emrh.Direct(
+    links = [Direct(
         "source-demand",
         nodes[1],
         nodes[2],
-        emrh.Linear()
+        Linear()
     )]
 
     #WIP(?) data structure
@@ -94,14 +103,14 @@ function run_case(op_number, demand_profile; case_config = "standard")
 
 
         cost_to_go = 500 #this should obviously be changed - it should be a function taking some input (e.g. storage capacity at the end of the operational period)
-        m = emrh.create_model(case, model; check_timeprofiles)
-        emrh.update_objective(m, cost_to_go)
+        m = create_model(case, model; check_timeprofiles)
+        update_objective(m, cost_to_go)
         jp.set_optimizer(m, optimizer)
         jp.set_optimizer_attribute(m, jp.MOI.Silent(), true)
         jp.optimize!(m)
     elseif case_config == "cost_to_go_func" #use the defined cost_to_go_func
         check_timeprofiles=true
-        m = emrh.create_model(case, model; check_timeprofiles)
+        m = create_model(case, model; check_timeprofiles)
 
         #optimization variable at the end of the operating period
         vars_ref_emb = case[:nodes]
@@ -109,12 +118,12 @@ function run_case(op_number, demand_profile; case_config = "standard")
 
         cost_to_go = cost_to_go_func(opt_vars_input)
 
-        emrh.update_objective(m, cost_to_go)
+        EMRH.update_objective(m, cost_to_go)
         jp.set_optimizer(m, optimizer)
         jp.set_optimizer_attribute(m, jp.MOI.Silent(), true)
         jp.optimize!(m)
     elseif case_config == "standard" #No cost-to-go: it is as in standard EMB
-        m = emrh.run_model(case, model, optimizer)
+        m = run_model(case, model, optimizer)
     else
         throw(MethodError(case_config, "Not implemented"))
     end
