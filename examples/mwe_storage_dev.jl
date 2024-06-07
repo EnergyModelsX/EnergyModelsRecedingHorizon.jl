0c15399e5e3d628a68820f45fa49e65e0a2f0558
Lucas Ferreira Bernardino <lucas.bernardino@sintef.no>
2024-06-07 11:32:56 +0200

    Included quadratic objective on storage level - solution with trade-off

--- examples/mwe_storage_dev.jl
+++ examples/mwe_storage_dev.jl
@@ -1,29 +1,16 @@
 using Pkg
 
-# Activate the local environment including EnergyModelsBase, HiGHS, PrettyTables
-# Pkg.activate(@__DIR__)
-# Install the dependencies.
-Pkg.activate("test")
+Pkg.activate("test") # to use solvers (HiGHS, Ipopt, ...)
 
 using HiGHS
+# using Ipopt
 using PrettyTables
 import JuMP as JP
 import EnergyModelsBase as EMB
 import TimeStruct as TS
-# using EnergyModelsBase
-# using TimeStruct
 import EnergyModelsRecHorizon as EMRH
 
 silent_flag = true
-
-function cost_to_go_func(opt_vars_input::Vector{JP.VariableRef})
-    
-    #Any of these cost-to-go functions work
-    # cost_to_go = zeros(1, length(opt_vars_input))*opt_vars_input
-    cost_to_go = ones(1, length(opt_vars_input))*opt_vars_input    
-
-    return cost_to_go[1] #there is probably a more elegant way of converting a 1-element Vector into a number instead of using [1] (so that the number can be used in the objective function of the optimization problem)
-end
 
 function create_case(op_number, demand_profile, price_profile; case_config = "standard", init_state = 0)
     #Define resources with their emission intensities
@@ -45,8 +32,6 @@ function create_case(op_number, demand_profile, price_profile; case_config = "st
         1, #duration of strategic period
         operational_periods; #operational period
         op_per_strat)
-
-        T2 = TS.TwoLevel([operational_periods, operational_periods])
     else 
         #purely operational period. However, this does not work for some reason (check_timeprofiles fails)
         T = TS.SimpleTimes(op_number, op_duration)
@@ -84,7 +69,6 @@ function create_case(op_number, demand_profile, price_profile; case_config = "st
         EMB.RefSink(
             "electricity demand", #node ID or name
             TS.OperationalProfile(demand_profile), #demand in MW (time profile)
-            # OperationalProfile([20, 30, 40, 30]), #demand in MW (time profile)
             Dict(:surplus => TS.FixedProfile(0), :deficit => TS.FixedProfile(1e6)), #surplus and deficit penalty for the node in EUR/MWh
             Dict(power => 1), #energy demand and corresponding ratio
         ),
@@ -110,25 +94,14 @@ function create_case(op_number, demand_profile, price_profile; case_config = "st
 
     check_timeprofiles=true
     m = EMB.create_model(case, model; check_timeprofiles)
-    JP.@expression(m, cost_RH, -0*sum((m[:stor_level]))) # TODO: incentive to stay at given level? Quadratic problem?
-    # cost_RH = -100*sum((m[:stor_level]-2.5)) 
+    cost_RH = -10*sum(( m[:stor_level].data .- 2.0).^2) # JP.@expression(m, cost_RH, -10*sum(( m[:stor_level].data .- 2.0).^2) )
 
     #Can choose different ways of running the case study. 
-    if case_config == "cost_to_go_scalar" #we simply add a scalar to the cost function (the scalar is the cost to go)
+    if case_config == "cost_to_go"
         
-        cost_to_go = 0 #this should obviously be changed - it should be a function taking some input (e.g. storage capacity at the end of the operational period)
+        cost_to_go = 0 # can include a function taking some input (e.g. storage capacity at the end of the operational period)
         cost_RH += cost_to_go
 
-    elseif case_config == "cost_to_go_func" # does not work for storage
-
-        #optimization variable at the end of the operating period
-        vars_ref_emb = case[:nodes]
-        opt_vars_input = [m[:cap_use][vars_ref_emb[i],end] for i=1:length(vars_ref_emb)] 
-
-        cost_to_go = cost_to_go_func(opt_vars_input)
-        cost_RH += cost_to_go
-
-        EMRH.update_objective(m, cost_to_go)
     elseif case_config == "standard" #No cost-to-go: it is as in standard EMB
         nothing
     else
@@ -142,7 +115,7 @@ end
 
 #Check if we get the same solution by running i) the standard implementation already existing in EMB (i.e. we consider the full time-horizon in one optimization problem) and ii) the receding horizon implementation
 
-optimizer = JP.optimizer_with_attributes(HiGHS.Optimizer, JP.MOI.Silent() => silent_flag)
+optimizer = JP.optimizer_with_attributes(HiGHS.Optimizer, JP.MOI.Silent() => silent_flag) # , "tol" => 1.0e-10
 
 #all-in-one
 op_number = 8
@@ -170,18 +143,22 @@ println("Receding horizon implementation")
 n_hor = 2
 init_level_vec = zeros(op_number)
 init_level_vec[1] = x0
-out_rec_horizon = zeros(op_number) #store the solution of the receding horizon implementation here
-stor_rec_horizon = zeros(op_number) #store the solution of the receding horizon implementation here
-sol_rec_horizon = zeros(op_number) #store the solution of the receding horizon implementation here
-cost_rec_horizon = zeros(op_number) #store the solution of the receding horizon implementation here
+#vectors for storing the solution of the receding horizon implementation
+out_rec_horizon = zeros(op_number)
+stor_rec_horizon = zeros(op_number)
+sol_rec_horizon = zeros(op_number)
+cost_rec_horizon = zeros(op_number)
 for i = 1:(op_number-n_hor+1)
+    # updating inputs
     demand_hor = demand_profile[i:i+n_hor-1]
     price_hor = price_profile[i:i+n_hor-1]
-    # case_i, nodes_i, m_i = create_case(n_hor, demand_hor, price_hor, case_config = "cost_to_go_func", init_state=init_level_vec[i])
-    case_i, nodes_i, m_i = create_case(n_hor, demand_hor, price_hor, case_config = "cost_to_go_scalar", init_state=init_level_vec[i])
+    x0_hor = init_level_vec[i]
+    # creating and solving model
+    case_i, nodes_i, m_i = create_case(n_hor, demand_hor, price_hor, case_config = "cost_to_go", init_state=x0_hor)
     JP.set_optimizer(m_i, optimizer)
     JP.set_optimizer_attribute(m_i, JP.MOI.Silent(), silent_flag)
     JP.optimize!(m_i)
+    # storing parameters
     av_i, source_i, stor_i, sink_i = case_i[:nodes]
     power_i, co2_i = case_i[:products]
     sol_rec_horizon[i:i+n_hor-1] = JP.value.(m_i[:cap_use][source_i,:]).data
@@ -194,7 +171,7 @@ for i = 1:(op_number-n_hor+1)
     if i == 1
         println(ctg_objective)
     end
-end # TODO: include final state as initial to next
+end
 
 # @assert solution_full_problem == sol_rec_horizon
 println("\n\nReceding horizon source usage: $sol_rec_horizon")
