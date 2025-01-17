722e1a67c4ae8a664e43c999d0b5e8a9046312aa
Julian Straus <julian.straus@sintef.no>
2025-01-17 11:36:50 +0100

    Improvement of folder structure through replacing utils.jl with smaller files

---
+++ src/utils/identification.jl
@@ -0,0 +1,220 @@
+
+
+"""
+    _fields_with_operational_profile(n::Union{NetworkNode, Source, Sink})
+    _fields_with_operational_profile(n::Storage)
+    _fields_with_operational_profile(n::Availability)
+    _fields_with_operational_profile(n::EMB.Node)
+
+Function for returning the fields in a node containing an `OperationalProfile`.
+If no fields are found, it returns `Symbol[]`.
+
+!!! note
+    This function is currently not in use. Function is kept since it may be beneficial later.
+
+# Example
+
+```julia
+el = ResourceCarrier("el", 0.2)
+heat = ResourceCarrier("heat", 0.0)
+co2 = ResourceEmit("co2", 1.0)
+n = RefNetworkNode(
+    "my_id", :id
+    FixedProfile(1e12), # :cap
+    OperationalProfile([1,2]), # :opex_var
+    FixedProfile(0), # :opex_fixed
+    Dict(el => 1), # :input
+    Dict(heat => 1), # :output
+    [EmissionsProcess(Dict(co2 => OperationalProfile([2,2])))] # :data
+)
+_fields_with_operational_profile(n) # returns [:opex_var, :data]
+```
+"""
+function _fields_with_operational_profile(n::EMB.Node)
+    return [
+        fn for fn ∈ fieldnames(typeof(n)) if _has_field_operational_profile(getfield(n, fn))
+    ]
+end
+
+function _fields_with_operational_profile(n::Availability)
+    return Symbol[]
+end
+
+"""
+    _has_field_operational_profile(field::OperationalProfile)
+    _has_field_operational_profile(field::StrategicProfile)
+    _has_field_operational_profile(field::Vector{Data})
+    _has_field_operational_profile(field::Data)
+    _has_field_operational_profile(field::EMB.AbstractStorageParameters)
+    _has_field_operational_profile(field::Dict)
+    _has_field_operational_profile(field)
+
+Function for recursively checking if `field` contains an `OperationalProfile`, returning
+true or false
+
+!!! note
+    This function is currently not in use. Function is kept since it may be beneficial later.
+
+# Example
+
+```julia
+co2 = ResourceEmit("co2", 1.0)
+
+# The following calls return true
+_has_field_operational_profile(OperationalProfile([1]))
+_has_field_operational_profile(EmissionsProcess(Dict(co2 => OperationalProfile(profile))))
+_has_field_operational_profile(StorCapOpexFixed(OperationalProfile([1]), FixedProfile(0)))
+_has_field_operational_profile(Dict(:a => Dict(:b => Dict(:c => OperationalProfile([1])))))
+
+# The following calls return false
+_has_field_operational_profile(Dict(:a => Dict(:b => Dict(:c => FixedProfile(1)))))
+_has_field_operational_profile(EmissionsProcess(Dict(co2 => FixedProfile(2))))
+_has_field_operational_profile(EmptyData())
+_has_field_operational_profile(InitStorageData(4.0))
+
+_has_field_operational_profile(EmissionsEnergy(OperationalProfile([1])))
+# EmissionsEnergy accepts any inputs, but does not store `OperationalProfiles`
+
+# The following calls return an error
+_has_field_operational_profile(StrategicProfile([1]))
+_has_field_operational_profile(Dict(:a => StrategicProfile([1, 2])))
+```
+"""
+_has_field_operational_profile(field::OperationalProfile) = true
+function _has_field_operational_profile(field::StrategicProfile)
+    error("EMRH should not be used with strategic profiles")
+    return nothing
+end
+function _has_field_operational_profile(field::Vector{<:Data})
+    return any([_has_field_operational_profile(d) for d ∈ field])
+end
+function _has_field_operational_profile(field::Data)
+    return any([
+        _has_field_operational_profile(getfield(field, f)) for f ∈ fieldnames(typeof(field))
+    ])
+end
+function _has_field_operational_profile(field::Dict)
+    return any([_has_field_operational_profile(val) for (key, val) ∈ field])
+end
+function _has_field_operational_profile(field::EMB.AbstractStorageParameters)
+    return any([
+        _has_field_operational_profile(getfield(field, f)) for f ∈ fieldnames(typeof(field))
+    ])
+end
+_has_field_operational_profile(field) = false
+
+"""
+    _find_paths_operational_profile(n::Union{NetworkNode, Source, Sink, Storage})
+    _find_paths_operational_profile(field::Union{NetworkNode, Source, Sink, Storage},
+        current_path::Vector{Any}, all_paths::Vector{Any})
+    _find_paths_operational_profile(field::Vector{<:Data}, current_path::Vector{Any},
+        all_paths::Vector{Any})
+    _find_paths_operational_profile(field::Union{Data, EMB.AbstractStorageParameters},
+        current_path::Vector{Any}, all_paths::Vector{Any})
+    _find_paths_operational_profile(field::AbstractDict, current_path::Vector{Any},
+        all_paths::Vector{Any})
+    _find_paths_operational_profile(field::OperationalProfile, current_path::Vector{Any},
+        all_paths::Vector{Any})
+    _find_paths_operational_profile(field::StrategicProfile, current_path::Vector{Any},
+        all_paths::Vector{Any})
+    _find_paths_operational_profile(field::Any, current_path::Vector{Any},
+        all_paths::Vector{Any})
+
+Function for returning the fields in a node `n` containing an `OperationalProfile`, returning
+a list of the path. The list can be nested, depending on the number of `OperationalProfile`said
+within the type
+
+# Example
+
+```julia
+co2 = ResourceEmit("co2", 1.0)
+sink = RefSink(
+    "a_sink", # :id
+    FixedProfile(1e5), # :cap
+    Dict(:surplus => OperationalProfile(zeros(dim_t)),
+        :deficit => OperationalProfile(1e6*ones(dim_t))), # :penalty
+    Dict(heat => 1), # :input
+    [EmptyData(), EmissionsProcess(Dict(co2 => OperationalProfile(profile)))] # :data
+)
+
+EMRH._find_paths_operational_profile(sink)
+# returns a 3-element Vector{Any}:
+#  Any[:penalty, :deficit]
+#  Any[:penalty, :surplus]
+#  Any[:data, "idx_2", :emissions, co2]
+
+# The function can also be used for checking other `types`:
+all_paths = []
+current_path = Any[:a_path]
+a_dict = Dict(:a => Dict(:b1 => Dict(:c => OperationalProfile([1])),
+    :b2 => OperationalProfile([1]), :b3 => [1]))
+EMRH._find_paths_operational_profile(a_dict, current_path, all_paths)
+
+# all_paths are now a 2-element Vector{Any}: [Any[:a_path, :a, :b2], Any[:a_path, :a, :b1, :c]]
+```
+"""
+function _find_paths_operational_profile(n::EMB.Node)
+    all_paths = []  # To store the paths to lists
+    _find_paths_operational_profile(n, [], all_paths)
+    return all_paths
+end
+function _find_paths_operational_profile(
+    field::EMB.Node,
+    current_path::Vector{Any},
+    all_paths::Vector{Any},
+)
+    for f ∈ fieldnames(typeof(field))
+        new_path = vcat(current_path, f)
+        _find_paths_operational_profile(getfield(field, f), new_path, all_paths)
+    end
+end
+function _find_paths_operational_profile(
+    field::Vector{<:Data},
+    current_path::Vector{Any},
+    all_paths::Vector{Any},
+)
+    for (i, d) ∈ enumerate(field)
+        new_path = vcat(current_path, ["idx_$(i)"])
+        _find_paths_operational_profile(d, new_path, all_paths)
+    end
+end
+function _find_paths_operational_profile(
+    field::Union{Data,EMB.AbstractStorageParameters},
+    current_path::Vector{Any},
+    all_paths::Vector{Any},
+)
+    for f ∈ fieldnames(typeof(field))
+        new_path = vcat(current_path, f)
+        _find_paths_operational_profile(getfield(field, f), new_path, all_paths)
+    end
+end
+function _find_paths_operational_profile(
+    field::AbstractDict,
+    current_path::Vector{Any},
+    all_paths::Vector{Any},
+)
+    for (key, value) ∈ field
+        new_path = vcat(current_path, key)
+        _find_paths_operational_profile(value, new_path, all_paths)
+    end
+end
+function _find_paths_operational_profile(
+    field::OperationalProfile,
+    current_path::Vector{Any},
+    all_paths::Vector{Any},
+)
+    push!(all_paths, current_path)  # Add current_path to all_paths
+end
+function _find_paths_operational_profile(
+    field::StrategicProfile,
+    current_path::Vector{Any},
+    all_paths::Vector{Any},
+)
+    error("EMRH should not be used with strategic profiles")
+end
+function _find_paths_operational_profile(
+    field::Any,
+    current_path::Vector{Any},
+    all_paths::Vector{Any},
+)
+end
---
+++ src/utils/results.jl
@@ -0,0 +1,142 @@
+"""
+    get_results(m::JuMP.Model)
+
+Function returning the values of the optimized model `m`. Some types are, however, not
+supported, and the function prints a warning message for those types and does not extract
+its value.
+"""
+function get_results(m::JuMP.Model)
+    res = Dict{Symbol, AbstractArray{<:Real}}()
+    for key in keys(object_dictionary(m))
+        val = _get_values_from_obj(m[key], key)
+        if ! isnothing(val)
+            res[key] = val
+        end
+    end
+    return res
+end
+
+function _get_values_from_obj(
+    obj::Union{JuMP.Containers.SparseAxisArray, JuMP.Containers.DenseAxisArray},
+    key::Symbol
+    )
+    return value.(obj)
+end
+function _get_values_from_obj(
+    obj::Union{EMB.SparseVariables.IndexedVarArray, EMB.SparseVariables.SparseArray},
+    key::Symbol,
+)
+    @warn "Extracting values from $(typeof(obj)) is not yet supported. Return nothing for $(key)"
+    return nothing
+end
+
+"""
+    update_results!(results, m, case_rh, case, 𝒽)
+
+Update results dictionary `results` given the optimization results `m`. `m` was optimized using the
+problem definition in `case_rh`, which is a slice of the original problem defined by `case`
+at the time period `𝒽`. The containers in `results` are indexed by the elements in `case`.
+"""
+function update_results!(results, m, case_rh, case, 𝒽)
+    𝒯ᴿᴴₒᵤₜ = optimization_time_ref(case[:T], 𝒽)
+    results_rh = get_results(m)
+    convert_dict = Dict(
+        n_rh => n for sym ∈ [:nodes, :links, :products] for
+        (n, n_rh) ∈ zip(case[sym], case_rh[sym])
+    ) # depends on elements being in same order
+    if isempty(results)
+        # allocate space in results
+        for (k, container_rh) ∈ results_rh
+            new_container = initialize_container(container_rh, convert_dict, case[:T])
+            if !isnothing(new_container)
+                results[k] = new_container
+            else
+                @warn "Ignoring result field $k"
+            end
+        end
+    end
+    # adding time structure to conversion dictionary - changes at each implementation step
+    for (tᴿᴴₐᵤₓ, tᴿᴴ) ∈ zip(case_rh[:T], 𝒯ᴿᴴₒᵤₜ)
+        convert_dict[tᴿᴴₐᵤₓ] = tᴿᴴ
+    end
+    # place values of results_rh into results
+    for (k, container) ∈ results
+        if isempty(results_rh[k])
+            continue
+        end
+        if typeof(container) <: Containers.DenseAxisArray
+            axes_new = tuple(
+                ([convert_dict[el] for el ∈ ax] for ax ∈ axes(results_rh[k]))...,
+            )
+            container[axes_new...] = results_rh[k].data
+        elseif typeof(container) <: Containers.SparseAxisArray
+            for (key, value) ∈ results_rh[k].data
+                key_new = tuple((convert_dict[ax] for ax ∈ key)...)
+                container[key_new...] = value
+            end
+        end
+    end
+end
+
+"""
+    initialize_container(container_rh, convert_dict, 𝒯)
+
+Returns an empty container of the same type as `container_rh`, changing its indexing according
+to the mapping in `convert_dict`.
+
+This supports the following container types:
+- **`Containers.DenseAxisArray`**: An array is initialized for the whole period 𝒯.\n
+- **`Containers.SparseAxisArray`**: This only requires type initialization.\n
+"""
+function initialize_container(container_rh::Containers.DenseAxisArray, convert_dict, 𝒯)
+    # replace RH references for corresponding references of full problem
+    axes_full = []
+    for ax ∈ axes(container_rh)
+        axtype = eltype(ax)
+        if axtype <: Union{EMB.Node,EMB.Link,EMB.Resource}
+            ax_full = [convert_dict[el] for el ∈ ax]
+        elseif axtype <: TimeStruct.OperationalPeriod
+            ax_full = collect(𝒯) # allocate space for full horizon
+        else
+            @warn "Unsuported indexing of type $axtype"
+            return nothing
+        end
+        push!(axes_full, ax_full)
+    end
+    # if !isempty(axes_full)
+    new_container = Containers.DenseAxisArray{Float64}(undef, axes_full...)
+    # end
+    return new_container
+end
+function initialize_container(container_rh::Containers.SparseAxisArray, convert_dict, 𝒯)
+    # sparse arrays only get type allocation
+    emptydict = JuMP.OrderedDict{eltype(keys(container_rh.data)),Float64}()
+    new_container = Containers.SparseAxisArray(emptydict)
+    return new_container
+end
+function initialize_container(container_rh, convert_dict, 𝒯)
+    container_type = typeof(container_rh)
+    @warn "Unsuported container type $container_type"
+    return nothing
+end
+
+"""
+    save_results(model::Model; directory=joinpath(pwd(),"csv_files"))
+
+Saves the model results of all variables as CSV files. The model results are saved in a new
+directory.
+If no directory is specified, it will create, if necessary, a new directory "csv_files" in
+the current working directory and save the files in said directory.
+"""
+function save_results(model::Model; directory = joinpath(pwd(), "csv_files"))
+    vars = collect(keys(object_dictionary(model)))
+    if !ispath(directory)
+        mkpath(directory)
+    end
+    Threads.@threads for v ∈ vars
+        if !isempty(model[v]) && !isa(model[v], VariableRef)
+            fn = joinpath(directory, string(v) * ".csv")
+            CSV.write(fn, JuMP.Containers.rowtable(value, model[v]))
+        end
+    end
+end
